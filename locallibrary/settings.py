c7809f27f95fb9882c04d31d0038674ffcc4d9f7
denyzero1 <75045049+denyzero1@users.noreply.github.com>
2020-11-26 14:04:11 +0300

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#6cdmp+ui-kr#w6-yof-koy1i6!yfd
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
